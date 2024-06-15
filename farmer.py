d6670559d5717abf9d079c5c183122a7a790942b
jakzukbis <jakzuk1@gmail.com>
2024-06-15 23:26:29 +0200

    modified:   farmer.py Some minor changes Marketplace added roll_animal_dice probably will be removed

--- farmer.py
+++ farmer.py
@@ -1,10 +1,13 @@
 from collections import Counter
 import random
 
+players = []
+
 class Player:
     def __init__(self, id) -> None:
         self.id = id
         self.fields = []
+        self.animals = []
         if self.id == 0:
             pass    # Append field (nie znam numeracji)
         elif self.id == 1:
@@ -13,20 +16,23 @@ class Player:
             pass
         elif self.id == 3:
             pass
+        self.animal_rolled = False
     
     def roll_animal_dice(self):
         first = ["Wolf", "Fox", "Sheep", "Sheep", "Cow", "Horse"] + ["Rabbit"] * 6
         second = ["Wolf", "Fox", "Cow"] + ["Sheep"] * 3 + ["Rabbit"] * 6
         predator = [1, 1, 1, 2, 2, 4]
+        return [random.choice(first), random.choice(second)]
+
         roll = [random.choice(first), random.choice(second)]
         animals = self.get_animals()
         new_animals = {}
         if roll[0] == roll[1]:
             only_roll = roll[0]
             if only_roll in ["Fox", "Wolf"]:
-                random.choice(predator)
+                victim = random.choice(predator)
                 # Zjadanie
-                random.choice(predator)
+                victim = random.choice(predator)
                 # Zjadanie
             else:
                 animals[only_roll] += (animals[only_roll] + 2) // 2
@@ -41,25 +47,23 @@ class Player:
                 if animals.get(animal) > 0:
                     new_animals[animal] = (animals[animal] + 1) // 2
         # Rozmieszczanie zwierzątek
-        animals_to_place = []
-        for animal in new_animals.keys():
-            for i in new_animals[animal]:
-                animals_to_place.append(Animal(animal))
-                new_animals[animal] -= 1
-        # Yyy, tutaj jest pod górkę...
-        for animal in animals_to_place:
-            animal.place()  # To na razie nic nie robi a ma robić dużo, w tym interfejs, choose field!
-    
+        # animals_to_place = []
+        for animal_type in new_animals.keys():
+            for animal in new_animals[animal_type]:
+                self.place_animal(animal_type)
+
+        #         animals_to_place.append(Animal(animal))
+        #         new_animals[animal] -= 1
+        # # Yyy, tutaj jest pod górkę...
+        # for animal in animals_to_place:
+        #     self.place_animal(animal)
+  
     def get_animals(self):
-        free = 0
-        animals = {}
-        for field in self.fields:
-            content = field.show_field()
-            if content == 0:
-                free += 1
-            content = Counter(content)
-            animals = Counter(animals)
-            animals = dict(content + animals)
+        animals = {"Rabbit" : 0, "Sheep" : 0, "Pig" : 0, "Cow" : 0, "Horse" : 0}
+        for animal_type in animals.keys():
+            for animal in self.animals:
+                if animal.type == animal_type:
+                    animals[animal_type] += 1
         return animals
     
     def upgrade_chosen_field(self):
@@ -79,17 +83,20 @@ class Player:
                 field.upgrade()
             # Powtórzyć elif dla wartości 2 i 4
 
-    def open_marketplace(self):
-        pass
-
     def remove_animal(self, animal_type):
         # klikanie
         chosen_field = self.choose_field()
         if animal_type in chosen_field.show_field().keys():
-            chosen_field.animals.pop()
+            removed_animal = chosen_field.animals.pop()
             chosen_field.check_capacity()
         else:   # Wypisze gdzie indziej, na razie konsola
             print(f"There is no {animal_type} in this field")
+
+        if removed_animal.space_needed == 2:
+            for field in removed_animal.fields:
+                field.animals.pop()
+                field.check_capacity
+        del removed_animal
 
     def place_animal(self, animal_type):
         # klikanie
@@ -102,17 +109,30 @@ class Player:
         if iter == len(self.fields):
             print("There is no space for another animal!")
             return 0
-        chosen_field = self.choose_field()
-        for i in range(animal.space_needed / 6):    #if i == 2 and NEIGHBOURS - zmodyfikować dla konia (nie mogą być dowolne trzy)
-            while chosen_field.capacity < animal.space_needed:
-                while chosen_field not in self.fields:
-                    chosen_field = self.choose_field()
-                chosen_field.animals.append(animal)
-                animal.place(chosen_field)
-                chosen_field.check_capacity()
+        
+        chosen_field = self.choose_field()    
+        while chosen_field.capacity < animal.space_needed:
+            while chosen_field not in self.fields:
+                chosen_field = self.choose_field()
+            chosen_field.animals.append(animal)
+            animal.place(chosen_field)
+            chosen_field.check_capacity()
 
-    def buy_field(self, id):
+        if animal.space_needed == 2:
+            second_chosen_field = self.choose_field()
+            while chosen_field.capacity < animal.space_needed:
+                while second_chosen_field not in chosen_field.neighbours:
+                    second_chosen_field = self.choose_field()
+                second_chosen_field.animals.append(animal)
+                animal.place(second_chosen_field)
+                second_chosen_field.check_capacity()
+
+    def replace_animal(self):
         pass
+
+    def choose_field(self):
+        pass
+    
 
 class Animal:
     def __init__(self, animal_type) -> None:
@@ -130,7 +150,7 @@ class Animal:
             self.space_needed = 12
             self.value = 24
         elif animal_type == "Horse":
-            self.space_needed = 18
+            self.space_needed = 12
             self.value = 48
         self.fields = []
 
@@ -138,10 +158,13 @@ class Animal:
         self.fields.append(field)
 
 class Field:
-    def __init__(self, value) -> None:  # Pewnie brakuje ID, jestem zmęczony i nie wiem 
+    def __init__(self, value, x, y) -> None: 
         self.value = value
         self.capacity = 6
         self.animals = []
+        self.coords = x, y
+        self.neighbours = check_neighbours(x, y)
+        self.owner = None
 
     def upgrade(self):
         field_values = [1, 2, 4, 5]
@@ -149,7 +172,7 @@ class Field:
 
     def show_field(self):
         if self.animals == []:
-            return 0
+            return "No animals on that field"
         else:
             name = self.animals[0].type
             number = len(self.animals)
@@ -161,18 +184,17 @@ class Field:
             actual_capacity += animal.space_needed
         self.capacity = 6 - actual_capacity
     
-class Marketplace:
+class Marketplace:  # Pamięta aktywnego gracza i listę graczy
     def __init__(self, active) -> None:
         self.buy_prices = {"Rabbit" : 1, "Sheep" : 6, "Pig" : 12, "Cow" : 24, "Horse" : 48}    
-        # self.sell_prices = {"Rabbit" : 0.75, "Sheep" : 4, "Pig" : 8, "Cow" : 16, "Horse" : 32}    # pieniądze
         self.player = active
 
     # Przyciski muszą być
     def exchange(self, first_type, second_type):
-        new_animal = Animal(second_type)
         player_animals = self.player.get_animals()
         if first_type not in player_animals.keys():
             print(f"You don't have {first_type}!")
+            return 0
 
         price = self.buy_prices.get(second_type)
         first_val = self.buy_prices.get(first_type)
@@ -186,21 +208,67 @@ class Marketplace:
             self.player.place_animal(second_type)
 
         if price < first_val:
-            # Tu coś o space_needed, żeby sprawdzać
-            self.player.remove_animal(first_type)
-            for i in range(first_val / price):
-                self.player.place_animal(second_type)
+            if first_type not in player_animals.keys():
+                print(f"You don't have {first_type}")
+                return 0
+            else:
+                self.player.remove_animal(first_type)
+                for i in range(first_val / price):
+                    self.player.place_animal(second_type)
 
-    # def buy_field(self, value):
-    #     chosen_field = self.player.choose_field()
-    #     for player in players
-    #         while chosen_field in self.player.fields:
-    #             chosen_field = self.player.choose_field()
+    def buy_field(self):
+        chosen_field = self.player.choose_field() 
+
+        while any(chosen_field in player.fields for player in players):    # Sprawdza czy pole już do kogoś należy
+            print("Field is already owned")
+            chosen_field = self.player.choose_field()
+        
+        price = chosen_field.value
+        if self.player.check_animals["Rabbit"] < price: # Sprawdza czy gracz dysponuje odpowiednią liczbą królików do zakupu
+            print("You have too little rabbits")
+            return 0
+        else:
+            for i in range(price):
+                self.player.remove_animal("Rabbit")
+            self.player.fields.append(chosen_field)
+
+    def upgrade_field(self):
+        chosen_field = self.player.choose_field()
+        while chosen_field not in self.player.fields:
+            print("It's not your field")
+            chosen_field = self.player.choose_field()
+        if chosen_field.value == 1:
+            price = 4
+        elif chosen_field.value == 2:
+            price = 10
+        elif chosen_field.value == 4:
+            price = 18
+        else:
+            print("You can't upgrade further!")
+        if self.player.check_animals["Rabbit"] < price:
+            print("You have too little rabbits")
+            return 0
+        else:
+            for i in range(price):
+                    self.player.remove_animal("Rabbit")
+            chosen_field.upgrade()
+
 
 
 def choose_field():
     chosen_field = Field(1) # WIP, domyślnie ma klikać
     return chosen_field   
 
+def check_neighbours(x, y):
+    neighbours = []
+    if x < 8:
+        neighbours.append((x + 1, y))
+    if x > 1:
+        neighbours.append((x - 1, y))
+    if y < 8:
+        neighbours.append((x, y + 1))
+    if y > 1:
+        neighbours.append((x, y - 1))
+    return neighbours
 
 player = Player(0)
